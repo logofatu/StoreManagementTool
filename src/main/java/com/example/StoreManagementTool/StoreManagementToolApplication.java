97c7460ff7301b5fd00157405b6e5da2800f0c18
Galina Logofatu <galina.logofatu@gmail.com>
2022-06-12 19:59:23 +0300

    create spring boot project

---
+++ src/main/java/com/example/StoreManagementTool/StoreManagementToolApplication.java
@@ -0,0 +1,15 @@
+package com.example.StoreManagementTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementToolApplication.class, args);
+		System.out.println("GLO");
+		System.out.println("GLO1");
+	}
+
+}
