e2c08e7e3f15af82e8eb899ff3cf4efe9506fc23
Galina Logofatu <galina.logofatu@gmail.com>
2022-06-12 22:10:59 +0300

    correct errors in order to start project

---
+++ java/com/example/StoreManagementTool/StoreManagementTool1Application.java
@@ -0,0 +1,13 @@
+package com.example.StoreManagementTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementTool1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementTool1Application.class, args);
+	}
+
+}
